54b5ea8a7659155023846f9808aa19390658216d
eleanorwong <eleanorwong@live.ca>
2016-02-19 11:04:25 -0800

    Committing run.py file

---
+++ run.py
@@ -0,0 +1,8 @@
+## IF CAN CONNNECT TO THE INTERNET
+# push download count to real server
+# run query to get database files for our box "SELECT * with BoxID = ourID"
+# write that query into local database (update) uid (sqlite)
+# pull files from s3 (if lastupdated earlier than today)
+# DONE
+
+# server runs
