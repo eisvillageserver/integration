722f3043b6f0661360f1af3bd04a93c1527a3419
Sunny <sunny.hl.leung@gmail.com>
2016-02-23 16:25:24 -0800

    Download from S3
    Download file given UUID in string form and bucket object

--- test.sample.py
+++ test.sample.py
@@ -7,22 +7,38 @@ import urllib
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 
-bucket_name= 'eisvillageserver'
+#CONNECT TO S3
 conn=boto.s3.connect_to_region("us-west-2",
        aws_access_key_id=AWS_ACCESS_KEY_ID,
        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
        is_secure=True,
        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
        )
+bucket_name= 'eisvillageserver'
 bucket = conn.get_bucket(bucket_name,validate=False)
 
-#make sure testing exists in folder
-boto.s3.key.Key(bucket=bucket, name='0/Image/V1uofC1ix-dakjlkdf').get_contents_to_filename("testing\\test.csv")
+#Input string format: boxID/Folder/UUID-file.ext
+#Output: UUID, None if UUID doesn't exists
+def getKeyUUID(key):
+    keyInfo=re.split(r'[/,-]',key.key)
+    if ( len(keyInfo) > 2):
+        return keyInfo[2]
+    else:
+        return None
+#Input: string UUID, bucket object from S3
+#Returns True if success, else False
+def download_S3(UUID,bucket):
+    for item in bucket:
+        if ( getKeyUUID(item) == UUID ): # download file if there exists an UUID
+            folder=os.path.dirname(item.key)
+            if not os.path.exists(folder): #make new directory only if it doesnt exist
+                os.makedirs(folder)
+            boto.s3.key.Key(bucket=bucket, name=item.key).get_contents_to_filename(str(item.key)) #download it
+            updated= True #update db from kenya's side
+            return True
+    return False
 
-for l in bucket:
-    print(l.key)
-    #Select only keys we want
-    if ('0/Image/V1uofC1ix-dakjlkdf' == str(l.name)):
-        url=str(l.generate_url(expires_in=60, query_auth=False, force_http=True))
-        testfile = urllib.URLopener()
-        testfile.retrieve(url, "test.png")
+print("Should return true:")
+print(download_S3('V1uofC1ix',bucket)) #Should be True
+print("Should return false")
+print(download_S3('random',bucket)) #Should be False
