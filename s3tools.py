6f6e5d15bb6627f0b7898cbb3f8dec00025f99cc
eleanorwong <eleanorwong@live.ca>
2016-03-08 14:08:22 -0800

    Sync local and cloud backend

---
+++ s3tools.py
@@ -0,0 +1,42 @@
+import boto3
+import boto
+import sys,os
+import json
+from boto.s3.connection import S3Connection
+import urllib
+import re
+
+with open('config.json') as c:
+    config = json.load(c)
+
+#CONNECT TO S3
+conn = boto.s3.connect_to_region("us-west-2",
+        aws_access_key_id = config["accessKeyId"],
+        aws_secret_access_key = config["secretAccessKey"],
+        is_secure = True,
+        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+       )
+
+bucket_name = 'eisvillageserver'
+bucket = conn.get_bucket( bucket_name, validate = False )
+
+# Input string format: boxID/Folder/UUID-file.ext
+# Output: UUID, None if UUID doesn't exists
+def getKeyUUID ( key ):
+    keyInfo = re.split(r'[/,-]', key.key)
+    if ( len(keyInfo) > 2 ):
+        return keyInfo[2]
+    else:
+        return None
+
+# Input: string UUID
+# Returns True if success, else False
+def downloadKey ( UUID ):
+    for item in bucket:
+        if ( getKeyUUID(item) == UUID ): # download file if there exists an UUID
+            folder = os.path.dirname(item.key)
+            if not os.path.exists(folder): #make new directory only if it doesnt exist
+                os.makedirs(folder)
+            boto.s3.key.Key( bucket = bucket, name = item.key ).get_contents_to_filename(str(item.key)) #download it
+            return True
+    return False
---
+++ sync.py
@@ -0,0 +1,223 @@
+import os, sys
+from sqlalchemy import *
+import time
+import datetime
+import json
+import urllib2
+import sqlalchemy as sa
+
+import s3tools as s3t
+
+f = '%Y-%m-%d %H:%M:%S'
+
+tableData = MetaData()
+
+files = Table('files', tableData,
+    Column('UID', VARCHAR(255), primary_key=True),
+    Column('Title', VARCHAR(255)),
+    Column('Description', TEXT),
+    Column('DateUploaded', DATETIME),
+    Column('S3URI', VARCHAR(255)),
+    Column('Category', VARCHAR(255)),
+    Column('Language', VARCHAR(255)),
+    Column('Country', VARCHAR(255)),
+    Column('DownloadCount', Integer),
+    Column('LastUpdated', DATETIME),
+    Column('DownloadCountSynced', BOOLEAN),
+    Column('BoxID', Integer)
+)
+
+# Loading data from config file
+with open('config.json') as c:
+    config = json.load(c)
+
+# Get Local Database Engine
+def getLocalDatabase(sqliteFile):
+    local = create_engine('sqlite:///' + sqliteFile)
+    local.echo = False
+    return local
+
+# Get Cloud Database Engine
+def getCloudDatabase(user, pw, server, port):
+    cloud = create_engine('mysql+pymysql://' + user + ':' + pw + '@' + server + ':' + port)
+    cloud.echo = False
+    return cloud
+
+# Get the date where the box was last updated
+def getLastUpdatedDate(boxID, clouddb):
+    db = clouddb.connect()
+    sql = "SELECT LastUpdated from eisvillageserver.boxes WHERE boxID =" + str(boxID)
+    result = db.execute(sql)
+    for row in result:
+        return row[0];
+
+# Get the date where the box was last synced
+def getLastSyncedDate(boxID, clouddb):
+    db = clouddb.connect()
+    sql = "SELECT LastSynced from eisvillageserver.boxes WHERE boxID =" + str(boxID)
+    result = db.execute(sql)
+    for row in result:
+        return row[0];
+
+# For first time setup, create local sqlite database with correct columns (but empty)
+def createBlankLocalTable(localdb):
+    tableData.create_all(localdb)
+    return
+
+# Set the last synced date of the box to now
+def updateLastSynced(boxID, clouddb):
+    cloud = clouddb.connect()
+    sql = "UPDATE eisvillageserver.boxes  SET LastSynced = '" + datetime.datetime.now().strftime(f) + "' WHERE (BoxID = " + str(boxID) + ")";
+    print sql
+    cloud.execute(sql)
+
+    return
+
+# For first time setup, copy entire aws file information to local db and download all files
+def copyContentData(boxID, localdb, clouddb):
+    cloud = clouddb.connect()
+    local = localdb.connect()
+
+    # GET DATA
+    sql = "SELECT * from eisvillageserver.files WHERE boxID =" + str(boxID)
+    rows = cloud.execute(sql)
+    for row in rows:
+        local.execute(files.insert().values(**row))
+
+    resetDownloadCountSynced(localdb);
+
+    sql = "SELECT UID from files"
+    rows = local.execute(sql);
+
+    for row in rows:
+        s3t.downloadKey(row[0]);
+
+    updateLastSynced(boxID, clouddb)
+
+    return
+
+# Set the entire column for DownloadCountSynced to False
+def resetDownloadCountSynced(localdb):
+    local = localdb.connect()
+    sql = "UPDATE files SET 'DownloadCountSynced' = 0"
+    local.execute(sql)
+
+# Update download count to cloud
+def pushDownloadCount(localdb, clouddb):
+    local = localdb.connect()
+    cloud = clouddb.connect()
+
+    sql = "SELECT UID, DownloadCount from files WHERE DownloadCountSynced = 0"
+    rows = local.execute(sql)
+    for row in rows:
+        uid = row[0]
+        dlc = row[1]
+
+        update = "UPDATE eisvillageserver.files SET DownloadCount = " + str(dlc) + " WHERE (UID ='" + uid + "')";
+        print update
+        cloud.execute(update);
+
+        reset = "UPDATE files SET DownloadCountSynced = 1 WHERE (UID = '" + uid + "')"
+        local.execute(reset);
+
+    return
+
+# Pull new file information AND update file
+def pullContentData(boxID, localdb, clouddb):
+    lastSynced = getLastSyncedDate(boxID, clouddb)
+    cloud = clouddb.connect()
+    local = localdb.connect()
+
+    sql = "SELECT * from eisvillageserver.files WHERE LastUpdated >= '" + lastSynced.strftime(f) + "' AND BoxID = " + str(boxID)
+
+    result = cloud.execute(sql);
+
+    for row in result:
+        old = "SELECT 'S3URI' from files WHERE UID = '{0}'".format(row[0])
+        olddata = local.execute(old)
+
+
+        insert = "INSERT OR REPLACE INTO files ('UID', 'Title', 'Description', 'DateUploaded', 'S3URI', 'Category', 'Language', 'Country', 'DownloadCount', 'LastUpdated', 'DownloadCountSynced', 'BoxID')"
+        insert += "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}')".format(row[0], row[1], row[2], row[3].strftime(f), row[4], row[5], row[6], row[7], row[8], row[9].strftime(f), 0, boxID)
+
+        update = "update files set 'Title' = '{0}', 'Description' = '{1}', 'S3URI' = '{2}', 'Category' = '{3}' WHERE (UID = '{4}')".format(row[1], row[2], row[4], row[5], row[0])
+
+        print insert
+        #print update
+
+        inserted = local.execute(insert);
+        updated = local.execute(update);
+
+        if (olddata != row[4]): #if s3uri's don't match
+            s3t.downloadKey(row[0]);
+
+        #updated = local.execute(update);
+
+
+        #local.execute(sql)
+    updateLastSynced(boxID, clouddb)
+
+    return
+
+
+    # if last updated date online file is more recent than local file, pull info, download file
+    # set last updated to now for online and localdb
+
+# Delete files and database info if not present
+def deleteMissingFiles(boxID, localdb, clouddb):
+    local = localdb.connect()
+    cloud = clouddb.connect()
+    liveuid = "SELECT UID from eisvillageserver.files WHERE BoxID = " + str(boxID);
+
+    uidc = cloud.execute(liveuid)
+
+    localuid = "SELECT UID from files"
+
+    uidl = local.execute(localuid)
+
+    for uid in uidl:
+        localid = uid[0];
+        for i in uidc:
+            if localid == i[0]:
+                sql = "SELECT S3URI from files WHERE uid = '" + uid[0] + "'";
+                deletee = local.execute(sql);
+                for row in deletee:
+                    uri = row[0]
+                    #uri.replace('/', '\\')
+                    uri = uri.split("/")
+                    print uri
+                    try:
+                        os.remove(uri)
+                    except:
+                        pass
+                sql = "DELETE from files WHERE uid = '" + uid[0] + "'";
+                local.execute(sql)
+
+# IF CANNOT FIND -> Delete row, Delete File
+
+
+# FULL SYNC FUNCTION!
+def syncBoxes(boxID, localdb, clouddb):
+    deleteMissingFiles(boxID, localdb, clouddb)
+    pushDownloadCount(localdb, clouddb)
+    pullContentData(boxID, localdb, clouddb)
+    return
+
+
+cloud = getCloudDatabase(config["aws"]["user"], config["aws"]["password"], config["aws"]["host"], config["aws"]["port"])
+local = getLocalDatabase("eisvsfiles.db")
+
+#pullContentData(8, local, cloud)
+deleteMissingFiles(8, local, cloud)
+
+#copyContentData(8, local, cloud)
+
+#db_aws = create_engine('mysql://eisvillageserver:kenyaeleanorsunny16@eisvillageserver.c0rrbiu1schc.us-west-2.rds.amazonaws.com:3306')
+
+
+#db_aws.connect();
+
+# FUNCTIONS
+# - SYNC METADATA
+# - SYNC UPLOAD COUNT
+# -
