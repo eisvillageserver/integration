3bf1cfc049724ab3e07bff4eae31115f951532d3
eleanorwong <eleanorwong@live.ca>
2016-03-17 17:57:53 -0700

    Adding download count and application

--- api.py
+++ api.py
@@ -19,36 +19,42 @@ CORS(app)
 
 
 # Implementations of endpoints
-class Files(Resource):
-    def get(self, file_cat):
-        conn = e.connect()
-        sql = "SELECT * FROM files WHERE Category = " + '''"''' + file_cat + '''"'''
-        query = conn.execute(sql)
+## TODO: merge all the category endpoints
 
-        result = {'files': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return result
+class Latest(Resource):
+    def get(self):
+        conn = e.connect()
+        query = conn.execute('''SELECT * FROM files ORDER BY LastUpdated DESC LIMIT 0, 9''')
+        result = {'files': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
+        return result;
 
 class Categories(Resource):
     def get(self):
         conn = e.connect()
         query = conn.execute('''SELECT DISTINCT Category FROM files''')
         result = {'categories': [i[0] for i in query.cursor]}
-        print result
         return result
+
+class Applications(Resource):
+    def get(self):
+        conn = e.connect()
+        query = conn.execute('''SELECT * FROM files where Category == "Application"''')
+        result = {'files': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
+        return result;
 
 class Documents(Resource):
     def get(self):
         conn = e.connect()
         query = conn.execute('''SELECT * FROM files where Category == "Document"''')
         result = {'files': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
-        return result;
+        return result
 
 class Images(Resource):
     def get(self):
         conn = e.connect()
         query = conn.execute('''SELECT * FROM files where Category == "Image"''')
         result = {'files': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
-        return result;
+        return result
 
 class Videos(Resource):
     def get(self):
@@ -64,23 +70,29 @@ class Music(Resource):
         result = {'files': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
         return result
 
-class FilesByID(Resource):
-    def get(self, numid):
+class Apps(Resource):
+    def get(self):
         conn = e.connect()
-        sql = "SELECT * FROM files WHERE UID = \'" + numid + "'"
-        print sql
-        query = conn.execute(sql)
+        query = conn.execute('''SELECT * FROM files where Category == "Application"''')
         result = {'files': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}
         return result
 
+class Count(Resource):
+    def post(self, uid):
+        conn = e.connect()
+        sql = "UPDATE files SET 'DownloadCount' = DownloadCount+1,'DownloadCountSynced'=0 WHERE UID = '" + uid + "'"
+        query = conn.execute(sql)
+        return "Downloaded!"
+
 # List of all our endpoints
-#api.add_resource(Files, '/files/<string:file_cat>', endpoint='files')
-api.add_resource(FilesByID, '/files/<string:numid>', endpoint='FilesById')
+api.add_resource(Count, '/count/<string:uid>')
 api.add_resource(Categories, '/categories/')
 api.add_resource(Documents, '/categories/documents')
 api.add_resource(Images, '/categories/images')
 api.add_resource(Videos, '/categories/videos')
 api.add_resource(Music, '/categories/music')
+api.add_resource(Applications, '/categories/applications')
+api.add_resource(Latest, '/latest/')
 
 @app.route("/")
 def main():
@@ -88,4 +100,4 @@ def main():
 
 # Run the API
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
